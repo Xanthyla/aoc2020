5b835da766eab0ffbbadc09c47bb8a4c654d70e2
Xanthyla <50047392+Xanthyla@users.noreply.github.com>
2020-12-02 23:13:07 +0100

    Refactor validator methods
    Only a few minor improvements for now.

--- days/2/passwords.js
+++ days/2/passwords.js
@@ -1,39 +1,45 @@
 input = document.body.innerText.split("\n").filter((item) => item != "");
 
 function parseInput(input) {
-	x = [];
-	input.forEach(entry => { x.push(entry.split(/-|:| /).filter((item) => item != ""))}); // RegEx: Splits for '-', ':' or ' ' characters
+	var x = [];
+	var splitExp = new RegExp('-|:| ') // RegEx: Splits input file into array
+	input.forEach(entry => { x.push(entry.split(splitExp).filter((item) => item != ""))}); 
 	return x;
 }
 
-function checkValidity1(entry) {
-  var repl = new RegExp('[^'+entry[2]+']', "g") 
-  var testStr = entry[3].replaceAll(repl,'') // Remove all characters other than the one we're looking for
-  return !((testStr.length < entry[0]) || (testStr.length > entry[1]))
-}
+function checkPValidity(entry,method) {
+    var index1 = entry[0];
+    var index2 = entry[1];
+    var letter = entry[2];
+    var pw = entry[3];
+	
+    switch(method) {
+        case 1:	// Part 1 validator
+            var repl = new RegExp('[^' + letter + ']', "g") // RegExp setup
+            pw = pw.replaceAll(repl,'') // Remove all characters other than the one we're looking for
 
-function checkValidity2(entry) {
-  var indexOne = entry[0] - 1;
-  var indexTwo = entry[1] - 1;
+            return !((pw.length < index1) || (pw.length > index2))
+        case 2: // Part 2 validator
+            index1 -= 1; 
+	    index2 -= 1; // corporate doesn't know about index zero
 
-  return ((entry[3][indexOne] == entry[2]) ^ (entry[3][indexTwo] == entry[2]))
+            return ((pw[index1] == letter) ^ (pw[index2] == letter))
+        default:
+            return 0
+    }
 }
 
 p = parseInput(input);
 
-total1 = 0;
+total1 = 0, total2 = 0;
 for (i = 0; i < p.length; i++) {
-  if (checkValidity1(p[i])) {
-    total1++
-  };
+    if (checkPValidity(p[i],1)) {
+        total1++;
+    };
+    if (checkPValidity(p[i],2)) {
+        total2++;
+    };
 }
 
-total2 = 0;
-for (i = 0; i < p.length; i++) {
-  if (checkValidity2(p[i])) {
-    total2++
-  };
-}
-
-console.log("Valid passwords (Part 1): " + total1)
-console.log("Valid passwords (Part 2): " + total2)
+console.log("Valid passwords (Part 1): " + total1);
+console.log("Valid passwords (Part 2): " + total2);
